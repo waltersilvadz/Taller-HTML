652e4076186d6cbb382e0f95670f3d5cb61722af
Sala T13 <CSalaT13@uniboyaca.edu.co>
2022-03-19 16:58:45 -0500

    Punto 2 Parte 1

--- js/script.js
+++ js/script.js
@@ -4,4 +4,10 @@ $(document).ready(()=>{
         texto = $("#textoIngresado").val();
         $("#respuestaUno").html("Cantidad de Caracteres: "+texto.length);
     });
+
+    $("#formPuntoDos").submit((ev) => {
+        ev.preventDefault();
+        valor = $("#valorIngresado").val();
+        $("#respuestaDos").html("Tipo de Valor Ingresado: "+typeof(valor));
+    });
 });
